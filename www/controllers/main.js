a6432ebebd3c80e17c2ecd6d7fc6e3eac9f4db12
yuttpan <yuttpanin@gmail.com>
2016-08-16 16:32:53 +0700

    start

---
+++ www/controllers/main.js
@@ -0,0 +1,16 @@
+angular.module('starter')
+
+.controller('mainController',function ($scope,$state) {
+
+
+
+
+  $scope.myDataArray = [
+    {
+     
+name:'ตรวจสอบการได้รับวัคซีนจากโรงพยาบาลแก้งสนามนาง',
+
+    }]
+
+ 
+})
